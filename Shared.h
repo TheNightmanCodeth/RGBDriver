4082c01273ec085c3d75167efc1629b888a371f3
Joe Diragi <joyod3@gmail.com>
2019-04-09 14:13:27 -0400

    Begins IOKit userland client impl
    Using Apples own `SimpleUserClient` example project for implementing an IOUserClient

---
+++ Shared.h
@@ -0,0 +1,15 @@
+//
+//  Shared.h
+//  Definitions shared between kext and userland API
+//
+//  Created by Joe Diragi on 4/9/19.
+//  Copyright © 2019 TheNightmanCodeth. All rights reserved.
+//
+
+#ifndef Shared_h
+#define Shared_h
+
+// Data structure passed between the tool and the 1337 api. This structure and its fields need to have
+// the same size and alignment between the api, 32-bit processes, and 64-bit processes.
+
+#endif /* Shared_h */
